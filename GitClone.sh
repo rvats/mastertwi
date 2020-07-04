813689699a71fe5b5ebd06cc1a32a5fcc564105b
Rahul Vats <vats.rahul@gmail.com>
2020-07-03 22:12:04 -0500

    Update GitClone.sh
    Update GitClone.sh

--- GitClone.sh
+++ GitClone.sh
@@ -1,11 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+# import multiple remote git repositories to local CODE dir
 
-readarray array <<< $( cat "$@" )
+set -e
 
-mkdir -p ~/git && cd ~/git
+# settings / change this to your config
+remoteHost=example.com
+remoteUser=username
+remoteDir="~/repositories/"
+remoteRepos=$(ssh -l $remoteUser $remoteHost "ls $remoteDir")
+localCodeDir="${HOME}/CODE/"
 
-for element in ${array[@]}
+# if no output from the remote ssh cmd, bail out
+if [ -z "$remoteRepos" ]; then
+    echo "No results from remote repo listing (via SSH)"
+    exit
+fi
+
+# for each repo found remotely, check if it exists locally
+# assumption: name repo = repo.git, to be saved to repo (w/o .git)
+# if dir exists, skip, if not, clone the remote git repo into it
+for gitRepo in $remoteRepos
 do
-  echo "clonning $element"
-  git clone $element
+  localRepoDir=$(echo ${localCodeDir}${gitRepo}|cut -d'.' -f1)
+  if [ -d $localRepoDir ]; then 	
+		echo -e "Directory $localRepoDir already exits, skipping ...\n"
+	else
+		cloneCmd="git clone ssh://$remoteUser@$remoteHost/$remoteDir"
+		cloneCmd=$cloneCmd"$gitRepo $localRepoDir"
+		
+		cloneCmdRun=$($cloneCmd 2>&1)
+
+		echo -e "Running: \n$ $cloneCmd"
+		echo -e "${cloneCmdRun}\n\n"
+	fi
 done
+ 
