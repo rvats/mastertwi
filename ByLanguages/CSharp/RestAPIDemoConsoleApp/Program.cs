6f9dd4b2be9975e4c994e197227e59879d7699d4
Rahul Vats <v-rahvat@microsoft.com>
2020-12-16 20:55:23 -0800

    Adding TwitterAuthAPI

---
+++ ByLanguages/CSharp/RestAPIDemoConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace RestAPIDemoConsoleApp
+{
+    class Program
+    {
+        static string ConsumerKey= "*******************************************";
+        static string ConsumerKeySecret= "**************************************";
+        static string AccessToken = "*******************************************";
+        static string AccessTokenSecret = "****************************************";
+
+        static void Main(string[] args)
+        {
+            PostTweet();
+            Console.ReadKey();
+        }
+
+        async static void PostTweet()
+        {
+            var twitter = new TwitterApi(ConsumerKey, ConsumerKeySecret, AccessToken, AccessTokenSecret);
+            var response = await twitter.Tweet("The bill #HR1044 is about America first, secondary it's an immigration bill. " +
+                "As it protects American worker first, stops visa abuse, not increasing single green card, " +
+                "doesn't harm NIW but solves the problem of hundreds of year green card backlog. Don't delay. #PassHR1044");
+            Console.WriteLine(response);
+        }
+    }
+}
