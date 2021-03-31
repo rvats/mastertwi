8f6a017d30d4b60d6d4de587569e582423bbf519
Rahul Vats <v-rahvat@microsoft.com>
2021-03-31 07:20:53 -0700

    Updating Code Base

---
+++ ByLanguages/CSharp/GalaxyClass/Galaxy/Galaxy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static partial class GalaxyClass
+{
+    public class Galaxy
+    {
+        public String Name { get; set; }
+        public int MegaLightYears { get; set; }
+    }
+}
---
+++ ByLanguages/CSharp/GalaxyClass/Galaxy/GalaxyClass.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+public static partial class GalaxyClass
+{
+    public static void ShowGalaxies()
+    {
+        var theGalaxies = new Galaxies();
+        foreach (Galaxy theGalaxy in theGalaxies.NextGalaxy)
+        {
+            Debug.WriteLine(theGalaxy.Name + " " + theGalaxy.MegaLightYears.ToString());
+        }
+    }
+}
