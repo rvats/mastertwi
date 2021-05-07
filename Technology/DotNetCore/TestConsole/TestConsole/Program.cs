7d24221dbfe8fe27ccc3ebf1dc3b79e7c03e76f4
Rahul Vats <vats.rahul@gmail.com>
2021-05-07 15:14:35 -0500

    Adding New Files

---
+++ Technology/DotNetCore/TestConsole/TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        private static string result;
+
+        static async Task Main()
+        {
+            await SaySomething();
+            Console.WriteLine(result);
+        }
+
+        static async Task<string> SaySomething()
+        {
+            result = "Hello world!";
+            await Task.Delay(5000);
+            return "Something";
+        }
+    }
+}
